808cb5312db077befa0f6cb1cb417ce88306d230
kishan-aghera <55836816+kishan-aghera@users.noreply.github.com>
2023-08-23 14:57:26 +0530

    Form created without react-hook-form and zod.

--- components/FormWithoutReactHookFormAndZod.tsx
+++ components/FormWithoutReactHookFormAndZod.tsx
@@ -1,17 +1,71 @@
-const FormWithoutReactHookFormAndZod = () => {
-  return (
-    <form className="flex flex-col gap-y-2">
-      <h1 className="text-3xl mb-8">Form React</h1>
+"use client";
 
-      <input type="email" placeholder="Email" className="px-4 py-2 rounded" />
+import { FormEvent, useState } from "react";
+
+const FormWithoutReactHookFormAndZod = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [errors, setErrors] = useState<string[]>([]);
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setIsSubmitting(true);
+
+    if (password !== confirmPassword) {
+      setErrors(["Password & Confirm Password must match!"]);
+      setIsSubmitting(false);
+      return;
+    }
+
+    console.log("Form submitted");
+
+    setErrors([]);
+    setEmail("");
+    setPassword("");
+    setConfirmPassword("");
+    setIsSubmitting(false);
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="flex flex-col gap-y-2">
+      <h1 className="text-3xl mb-8 max-w-xs">
+        Form React without React hook form
+      </h1>
+
+      {errors.length > 0 && (
+        <ul className="max-w-xs">
+          {errors.map((error) => (
+            <li
+              key={error}
+              className="bg-red-100 text-red-500 px-4 py-2 rounded"
+            >
+              {error}
+            </li>
+          ))}
+        </ul>
+      )}
 
       <input
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        type="email"
+        placeholder="Email"
+        className="px-4 py-2 rounded"
+      />
+
+      <input
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
         type="password"
         placeholder="Password"
         className="px-4 py-2 rounded"
       />
 
       <input
+        value={confirmPassword}
+        onChange={(e) => setConfirmPassword(e.target.value)}
         type="password"
         placeholder="Confirm Password"
         className="px-4 py-2 rounded"
@@ -19,7 +73,8 @@ const FormWithoutReactHookFormAndZod = () => {
 
       <button
         type="submit"
-        className="bg-blue-500 disabled:bg-gray-500 py-2 rounded"
+        disabled={isSubmitting}
+        className="bg-blue-500 disabled:bg-gray-300 py-2 rounded"
       >
         Submit
       </button>
